cf40b90e4c845dd0f486c2367058ab7b94ff4f6d
Majeed <majeedk526@gmail.com>
2019-06-28 14:23:41 +0530

    parsing bug fixes

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ from train import Trainer
 from skip_gram import SkipGramModel
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', 'Data directory', default='./data')
-parser.add_argument('--embed_dim', 'Embedding dimension', type=int, default=100)
-parser.add_argument('--batch_size', 'Batch Size', type=int, default=10)
-parser.add_argument('--lr', 'Learning rate', type=float, default=0.001)
-parser.add_argument('--epochs', 'Epochs', type=int, default=5)
-parser.add_argument('--device', 'cpu/gpu', default='cpu')
+parser.add_argument('--data_dir', help='Data directory', default='./data')
+parser.add_argument('--embed_dim', help='Embedding dimension', type=int, default=100)
+parser.add_argument('--batch_size', help='Batch Size', type=int, default=10)
+parser.add_argument('--lr', help='Learning rate', type=float, default=0.001)
+parser.add_argument('--epochs', help='Epochs', type=int, default=5)
+parser.add_argument('--device', help='cpu/gpu', default='cpu')
 
 args = parser.parse_args()
-vocab_path = os.path.join(args.get('data_dir'), 'i2word.pkl')
-train_data_path = os.path.join(args.get('data_dir'),'train_data.npy')
-embed_dim = args.get('embed_dim')
-batch_size = args.get('batch_size')
-epochs = args.get('epochs')
-learning_rate = args.get('lr')
-device = 'cpu' if args.get('device') == 'cpu' else torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+vocab_path = os.path.join(args.data_dir, 'id2word.pkl')
+train_data_path = os.path.join(args.data_dir,'train_data.npy')
+embed_dim = args.embed_dim
+batch_size = args.batch_size
+epochs = args.epochs
+learning_rate = args.lr
+device = 'cpu' if args.device == 'cpu' else torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 skip_gram_dataset = SkipGramDataSet(train_data_path, vocab_path=vocab_path)
 data_loader = torch.utils.data.DataLoader(dataset=skip_gram_dataset,
